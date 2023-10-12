c6ef2b264ab4fd47feba848f350f3b09caa04456
Matthew Deutsch <87136927+mdeutsc2@users.noreply.github.com>
2023-10-11 21:54:58 -0600

    exporting env vars for desktop

---
+++ setcuda.bash
@@ -0,0 +1,8 @@
+export CHPL_LOCALE_MODEL=gpu
+export CHPL_CUDA_PATH=/usr/lib/cuda
+export CHPL_GPU=nvidia
+export CHPL_GPU_MEM_STRATEGY=unified_memory
+export CHPL_LLVM=bundled
+echo $CHPL_LOCALE_MODEL
+echo $CHPL_CUDA_PATH
+echo $CHPL_GPU
