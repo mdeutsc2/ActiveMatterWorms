54d67f7d09c682ba422adc2f1a22c085ec0d378b
Matthew Deutsch <87136927+mdeutsc2@users.noreply.github.com>
2023-10-25 00:26:44 -0600

    Update ovito_render.py

--- ovito_render.py
+++ ovito_render.py
@@ -1,4 +1,4 @@
-import math,os,argparse
+import math,os,argparse,multiprocessing,tqdm,time
 import numpy as np
 from ovito.io import import_file
 from ovito.modifiers import *
@@ -188,26 +188,54 @@ class Render:
                        background = (1.0,1.0,1.0),
                        stop_on_error=True)
 
-def main(args):
-    print(args.filename)
+def process_file(f,args):
+    start_time = time.time()
     debug = args.verbose
     if args.all == True:
         args.solvent = True
         args.worms = True
         args.velocity = True
-    for f in args.filename:
-        if args.worms:
-            Render.worm_positions(f,"test_anim_worms.mp4")
-            # Render.worm_positions(f,test_name)
-        if args.solvent:
-            Render.solvent_positions(f,"test_anim_solvent.mp4")
-            # Render.solvent_positions(f,test_name)
-        if args.all:
-            Render.all_positions(f,"test_anim_all.mp4")
-            # Render.all_positions(f,test_name)
-        if args.velocity:
-            print("velocity not done yet")
-            # Render.velocity_field(f)
+    if args.worms:
+        # Render.worm_positions(f,debug,"test_anim_worms.mp4")
+        Render.worm_positions(f,debug)
+    if args.solvent:
+        # Render.solvent_positions(f,debug,"test_anim_solvent.mp4")
+        Render.solvent_positions(f,debug)
+    if args.all:
+        # Render.all_positions(f,debug,"test_anim_all.mp4")
+        Render.all_positions(f,debug)
+    if args.velocity:
+        print("velocity not done yet")
+        # Render.velocity_field(f)
+    end_time = time.time()
+    total_time = end_time - start_time
+    if args.ncpus > 1:
+        process_name = multiprocessing.current_process().name
+        print(f"Processed {f} in {total_time} s on {process_name}",)
+    else:
+        print(f"Processed {f} in {total_time} s")
+
+def main(args):
+    print(args.filename)
+    if args.ncpus < 0:
+        args.ncpus = multiprocessing.cpu_count()
+
+    print("# cpus",args.ncpus)
+
+    # SERIAL
+    if args.ncpus > 1:
+        # PARALLEL
+        pool = multiprocessing.Pool(processes=ncpus)
+        #pool.starmap(process_file,tqdm.tqdm([(f,args) for f in args.filename],total=len(args.filename)))
+        pool.starmap(process_file,[(f,args) for f in args.filename])
+
+        pool.close()
+        pool.join()
+    else:
+        for f in args.filename:
+            process_file(f,args)
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -221,6 +249,7 @@ if __name__ == "__main__":
     parser.add_argument("-s","--solvent",help="only render solvent data",action="store_true")
     parser.add_argument("-w","--worms",help="only render worm data",action="store_true")
     parser.add_argument("-v","--velocity",help="only render velocity data",action="store_true")
+    parser.add_argument("-n","--ncpus",help="number of cpus for parallel data processing, default=0 (serial), -1=all cpus",default=0)
     parser.add_argument("--verbose",help="verbose logging",action="store_true")
     args = parser.parse_args()
     main(args)
