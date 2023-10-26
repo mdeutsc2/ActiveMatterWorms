c307c8bd87d23d45fca56642cc4868e36fe64256
Matthew Deutsch <87136927+mdeutsc2@users.noreply.github.com>
2023-10-25 23:05:30 -0600

    adding checksums

--- merge_xyz.py
+++ merge_xyz.py
@@ -1,6 +1,8 @@
 #!/bin/python3
 from os import listdir
 import glob
+import tqdm
+import hashlib
 import argparse
 
 def main():
@@ -14,18 +16,31 @@ def main():
     xyzfilelist.sort()
     #print(xyzfilelist)
 
-
+    print("reading data...")
     merged_file = open(input_string,"a")
-    for filename in xyzfilelist:
+    for filename in tqdm.tqdm(xyzfilelist):
         #for line in file.readline()
         file = open(filename,"r")
-        print(filename)
         data = file.read()
         file.close()
         merged_file.write(data)
 
     merged_file.close()
+    print("checking data...")
+    # Python program to find MD5 hash value of a file
+    md5_hash = hashlib.md5()
+    with open(input_string,"rb") as f:
+        # Read and update hash in chunks of 4K
+        for byte_block in tqdm.tqdm(iter(lambda: f.read(4096),b"")):
+            md5_hash.update(byte_block)
+            # print(md5_hash.hexdigest())
+
+    md5_file = input_string.split(".")[0] + ".md5"
+    with open(md5_file,"w+") as f:
+        f.write(md5_hash.hexdigest())
+
     print(input_string)
+    print(md5_hash.hexdigest()," > ",md5_file)
 
 if __name__ == '__main__':
     main()
