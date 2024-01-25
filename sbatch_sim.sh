3522d2bb85154b9cd0500561776bbecefaceb487
Matthew Deutsch <87136927+mdeutsc2@users.noreply.github.com>
2024-01-24 19:48:53 -0700

    Update sbatch_sim.sh

--- sbatch_sim.sh
+++ sbatch_sim.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=amatter_sim_chpl
 #SBATCH --account=pgs0213               # account for mdeutsc2
-#SBATCH --time=6:00:00                  # timeout for the batch job
+#SBATCH --time=72:00:00                  # timeout for the batch job
 #SBATCH --nodes=1                       # requesting number of nodes
 #SBATCH --ntasks-per-node=16            # requesting number of cpus/node
 #SBATCH --mail-type=END,FAIL            # send email to submitting user on job completion or failure
@@ -10,10 +10,11 @@
 set -e
 #loading modules
 module load cmake/3.25.2
-module load cuda/11.8.0
+#module load cuda/11.8.0
 module load gnu/12.3.0
 #executable section
-source /users/PGS0213/mdeutsc2/chapel-1.31.0/util/setchplenv.bash
+lscpu
+source /users/PGS0213/mdeutsc2/chapel-1.33.0_cpu/util/setchplenv.bash
 make clean
 make
 make run
